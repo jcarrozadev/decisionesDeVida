594a1928a6519eeb24776f2130da308b8634c17b
pcaLdito <pablogomezcaldito.guadalupe@alumando.fundacionloyola.net>
2024-12-03 18:14:44 +0100

    act

--- src/php/vistas/vAltaPersonaje.php
+++ src/php/vistas/vAltaPersonaje.php
@@ -35,6 +35,6 @@
             <input type="submit" value="Enviar">
         </form>
 
-        <script src="././js/vistas/validarAlta.js"></script>
+        <script src="src\js\vistas\validarAlta.js"></script>
     </body>
 </html>
