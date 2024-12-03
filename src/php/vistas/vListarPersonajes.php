6165e772bce857d9394f80e79abc6d469c7410b5
pcaldito <147615008+pcaldito@users.noreply.github.com>
2024-12-03 08:35:46 +0100

    vistas actualizado

--- src/php/vistas/vListarPersonajes.php
+++ src/php/vistas/vListarPersonajes.php
@@ -5,21 +5,23 @@
         <p>Decisiones de Vida</p>
     </div>
     <section class="contenedor-personajes">
-
         <?php
-            
-            foreach ($datos as $personaje) {
-                echo "<div class='fila-personaje'>";
-                    echo "<div class='icono-personaje' style='background-image: url(data:image/png;base64," . base64_encode($personaje['spriteFront']) . ")'></div>";
 
+            foreach($datos as $personaje) {
+
+                echo "<div class='fila-personaje'>";
+                    echo "<div class='icono-personaje'>";
+                        echo "<img src='data:image/png;base64," . base64_encode($personaje['spriteFront']) . "' />";
+                    echo "</div>";
                     echo "<p>" . $personaje['nombrePersonaje'] . "</p>";
                     echo "<div class='acciones'>";
                         echo "<a class='boton-modificar' href='index.php?c=personaje&m=modificarPersonaje&id=" . $personaje['idPersonaje'] . "'>Modificar</a>";
                         echo "<a class='boton-eliminar' href='index.php?c=personaje&m=eliminarPersonaje&id=" . $personaje['idPersonaje'] . "'>Eliminar</a>";
                     echo "</div>";
                 echo "</div>";
+
             }
-            
+
         ?>
     </section>
 </main>
