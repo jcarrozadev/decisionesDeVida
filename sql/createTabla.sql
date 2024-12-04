bbff67eb4142cd30143654798aaef784cb522d70
jcarrozadev <jariasc14@gmail.com>
2024-12-04 11:00:51 +0100

    Creación de tablas Script SQL + Inserción Escenarios

--- sql/createTabla.sql
+++ sql/createTabla.sql
@@ -1,19 +1,46 @@
-CREATE TABLE Personaje (
-    idPersonaje TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombrePersonaje VARCHAR(50) NOT NULL,
-    spriteFront BLOB NOT NULL,
-    spriteBack BLOB NOT NULL,
-    spriteLeft BLOB NOT NULL,
-    spriteRight BLOB NOT NULL,
-    PRIMARY KEY (idPersonaje)
+-- Javier Arias Carroza
+
+/* Creación de tablas */
+
+CREATE TABLE NPC (
+    idNPC TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombreNPC VARCHAR(20) NOT NULL UNIQUE,
+    sprite BLOB NOT NULL,
+    PRIMARY KEY (idNPC)
 );
 
-CREATE TABLE Jugador (
-    idJugador TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    tiempoTotal SMALLINT UNSIGNED NOT NULL,
-    dineroTotal SMALLINT NOT NULL,
-    idPersonaje TINYINT UNSIGNED NOT NULL,
-    CONSTRAINT PRIMARY KEY (idJugador),
-    CONSTRAINT fk_idPersonaje FOREIGN KEY (idPersonaje) REFERENCES Personaje(idPersonaje)
+CREATE TABLE Escenario (
+    idEscenario TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombreEscenario VARCHAR(30) NOT NULL,
+    nombreImagen VARCHAR(20) NOT NULL,
+    mensajeNarrativo VARCHAR(150) NOT NULL,
+    PRIMARY KEY (idEscenario)
+);
+
+CREATE TABLE Respuestas (
+    idRespuesta TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    mensaje VARCHAR(30) NOT NULL,
+    dinero VARCHAR(20) NOT NULL,
+    tiempo VARCHAR(150) NOT NULL,
+    idDiálogo SMALLINT UNSIGNED NOT NULL,
+    idEscenario TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idRespuesta),
+    FOREIGN KEY (idDiálogo) REFERENCES Diálogos(idDiálogo),
+    FOREIGN KEY (idEscenario) REFERENCES Escenario(idEscenario)
+);
+
+CREATE TABLE Diálogos (
+    idDiálogo SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombreDiálogo VARCHAR(20) NOT NULL,
+    mensaje VARCHAR(150) NOT NULL,
+    casilla CHAR(2) NOT NULL,
+    idNPC TINYINT UNSIGNED NOT NULL,
+    idRespuesta1 TINYINT UNSIGNED NOT NULL,
+    idRespuesta2 TINYINT UNSIGNED NOT NULL,
+    idEscenario TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idDiálogo),
+    FOREIGN KEY (idNPC) REFERENCES NPC(idNPC),
+    FOREIGN KEY (idRespuesta1) REFERENCES Respuestas(idRespuesta),
+    FOREIGN KEY (idRespuesta2) REFERENCES Respuestas(idRespuesta),
+    FOREIGN KEY (idEscenario) REFERENCES Escenario(idEscenario)
 );
