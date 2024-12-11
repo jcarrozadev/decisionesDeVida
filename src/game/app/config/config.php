eae01ce7c79e916ac7fc68860071860445770aaa
Miriam <mlopezvega.guadalupe@alumnado.fundacionloyola.net>
2024-12-11 10:16:34 +0100

    commit bueno del merge de main-mainMergeMiriam

--- src/game/app/config/config.php
+++ src/game/app/config/config.php
@@ -1,25 +1,16 @@
 <?php
 
     // ----------------------- CONTROLADOR Y METODO POR DEFECTO
-<<<<<<< HEAD
-    define('CONTROLADOR_DEFECTO', 'panelAdmin'); // cambiarlo
-    define('METODO_DEFECTO', 'inicio'); // cambiarlo
-=======
     define('CONTROLADOR_DEFECTO', 'jugar'); // cambiarlo
     define('METODO_DEFECTO', 'index'); // cambiarlo
->>>>>>> 57dd90fc1b7a7798f744124b81bc22f77bf7d264
 
     // ----------------------- RUTAS
     define('SPRITE_PATH', 'img/spritesPersonajes/');
     define('MODEL_PATH', 'app/modelos/');
     define('VIEW_PATH', 'app/vistas/');
     define('CONTROLLER_PATH', 'app/controladores/');
-<<<<<<< HEAD
-    define('ASSETS_PATH', 'app/vistas/assets/includes/');
-=======
 
     define('VIEW_JS_PATH', 'js/vistas/');
->>>>>>> 57dd90fc1b7a7798f744124b81bc22f77bf7d264
 
     // ----------------------- Tamaño máximo de SPRITES
     define('MAX_SPRITE_SIZE', 10000);
@@ -30,11 +21,5 @@
     // ----------------------- Tamaño máximo de dinero y tiempo
     define('DINERO_MAX', 100000);
     define('TIEMPO_MAX', 100000);
-<<<<<<< HEAD
-
-    // ----------------------- Rutas del CONFIG DB
-    define('CONFIG_PATH', 'app/config/');
-=======
->>>>>>> 57dd90fc1b7a7798f744124b81bc22f77bf7d264
     
 ?>
